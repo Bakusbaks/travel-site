3c887dde9b72bc82f9f4ed7cb927670bd47d80be
Jakub <jakubbakiera@MacBook-Air-Jakub.local>
2020-08-27 16:38:03 +0200

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -8,6 +8,24 @@ new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let mobileMenu = new MobileMenu();
 
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There was a problem"));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
+
 if (module.hot) {
   module.hot.accept();
 }
