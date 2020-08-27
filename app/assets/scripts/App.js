8c4b34ba783255a50f359da6f9207d85b1a6ce70
Jakub <jakubbakiera@macbook-air-jakub.home>
2020-08-27 14:41:15 +0200

    Completed sticky header functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import stickyHeader from "./modules/StickyHeader";
 
+let StickyHeader = new stickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let mobileMenu = new MobileMenu();
